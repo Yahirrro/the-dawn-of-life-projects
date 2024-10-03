4916c5158ade8bb03feac3c48225aacf090b3f60
Yahiro Nakamoto <yahiro0921@gmail.com>
2024-10-03 12:47:48 +0900

    Fix Sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,23 +1,19 @@
+// related: https://github.com/vercel/next.js/issues/67005
+
 import { MetadataRoute } from "next";
 import { getItems } from "~/components/util/getItems";
 
-// Adapt this as necessary
 const host = "https://the-dawn-of-life-projects.vercel.app";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  // Adapt this as necessary
   return [
     {
       url: host + "/ja",
       lastModified: new Date(),
-      alternates: {
-        languages: {
-          ja: host + "/ja",
-          en: host + "/en",
-        },
-      },
+      changeFrequency: "yearly",
+      priority: 1,
     },
-    ...(await itemsGenerator()),
+    ...((await itemsGenerator()) as any),
   ];
 }
 
@@ -26,11 +22,7 @@ const itemsGenerator = async () => {
   return items.map((item) => ({
     url: `${host}/ja/item/${item.id}`,
     lastModified: new Date(),
-    alternates: {
-      languages: {
-        ja: `${host}/ja/item/${item.id}`,
-        en: `${host}/en/item/${item.id}`,
-      },
-    },
+    changeFrequency: "monthly",
+    priority: 0.5,
   }));
 };
